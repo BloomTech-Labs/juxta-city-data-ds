a3e13fb7ac93be97240c68b0d0b7913006d4882a
AnikaZN <49991173+AnikaZN@users.noreply.github.com>
2020-03-03 20:11:46 -0700

    Updated CSV and added descriptions

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, request, json
 from flask_cors import CORS
 import pandas as pd
 
-df = pd.read_csv('./JuxtaCityData3.csv')
+df = pd.read_csv('./recleanedcitydata.csv')
 
 app = Flask(__name__)
 CORS(app)
@@ -30,7 +30,55 @@ def all_data():
     city = df[df['city'] == name]
     output = city.to_dict('records')
     data = output[0]
-    return jsonify(data)
+
+    name = data['City_Name']
+
+    # Population
+    pop = data['population']
+    change = data['population_change']
+    density = data['Population_Density']
+    age = data['Median_Age']
+    # Economy
+    houseinc = data['Median_Income']
+    capitainc = data['per_capita_Income']
+    poverty = data['Percent_below_Poverty']
+    industry = data['Most_Common_Industries']
+    # Climate
+    aqi = data['AQI']
+    weather = 'to be added'
+    # Cost of Living
+    cli = data['Cost_of_Living_Index']
+    house = data['Median_House_Value']
+    hdti = data['HDTI']
+    tax = data['Property_taxes']
+    rent = data['Median_Rent']
+    rti = data ['RTI']
+    drive = data['Average_Commute_Time']
+
+    # When relative words are needed
+    if change < 0:
+        growth = 'decrease'
+    else:
+        growth = 'increase'
+
+    if density < 1304:
+        rise = 'lower'
+    elif density == 1304:
+        rise = 'equal to'
+    else:
+        rise = 'higher'
+
+    if cli < 51:
+        cli_rel = 'low'
+    else:
+        cli_rel = 'high'
+
+    population = f'The population is {pop} as of 2017, which is a {change}% {growth} since 2000. The population density is {density} people per square mile, which is {rise} than the optimal population density of 1304. Finally, the average resident age in {name} is {age} years old.'
+    economy = f'In {name}, the median household income is ${houseinc}. This means that, per person in the city, the average annual income is ${capitainc}. {poverty}% of people live below the poverty line. The most common industries are the following: {industry}.'
+    climate = f'{name} has an air quality score of {aqi} - remember, lower is better! The average weather is {weather}.'
+    living_cost = f"On a national scale, {name}'s cost of living is relatively {cli_rel}, with a score of {cli}. The median home value is ${house}, with a housing debt to income ratio of {hdti}. That includes the cost of property taxes, which is ${tax} on average. If you're a renter, you can expect a median cost of ${rent}, and the rent to income ratio is {rti}. Last but not least to consider, the average commute time is {drive} minutes."
+
+    return jsonify(data, population, economy, climate, living_cost)
 
 
 # Returns list of cities that start with given characters
