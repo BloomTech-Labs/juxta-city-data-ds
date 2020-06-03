aa39f55710752d191c27cd746fad04cccc0321c3
James Barciz <jamesjbarciz@gmail.com>
2020-06-03 14:14:28 -0400

    update pg_insert.py

--- pg_insert.py
+++ pg_insert.py
@@ -15,7 +15,7 @@ DB_HOST = os.getenv("DB_HOST")
 DB_PASS = os.getenv("DB_PASS")
 DB_USER = os.getenv("DB_USER")
 
-# Read in the csv from GitHub (or from parallel directory)
+# Read in the CSV files from GitHub (https://github.com/Lambda-School-Labs/juxta-city-data-ds/tree/heart-disease-data/useful_datasets)
 heart = pd.read_csv('https://raw.githubusercontent.com/Lambda-School-Labs/juxta-city-data-ds/heart-disease-data/useful_datasets/heart_data.csv')
 economy = pd.read_csv('https://raw.githubusercontent.com/Lambda-School-Labs/juxta-city-data-ds/heart-disease-data/useful_datasets/economy_data.csv')
 housing = pd.read_csv('https://raw.githubusercontent.com/Lambda-School-Labs/juxta-city-data-ds/heart-disease-data/useful_datasets/housing_data.csv')
