1f9e4a7ca7c6d06465ef8027c954dc3133ec11bf
James <jamesjbarciz@gmail.com>
2020-06-03 20:31:18 -0400

    attempt unit testing

--- test_main.py
+++ test_main.py
@@ -1,12 +1,29 @@
-from app import app
+import os
+import psycopg2
 import unittest
+import pandas as pd
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_NAME = os.getenv("DB_NAME")
+DB_HOST = os.getenv("DB_HOST")
+DB_PASS = os.getenv("DB_PASS")
+DB_USER = os.getenv("DB_USER")
+
+connection = psycopg2.connect(database=DB_NAME, user=DB_USER, 
+                              password=DB_PASS, host=DB_HOST, port="5432")
+cursor = connection.cursor()
+
+heart = pd.read_csv('./useful_datasets/heart_data.csv')
 
 class BasicTest(unittest.TestCase):
 
-    def root_test(self):
-        response = self.app.get("/", follow_redirects=True)
-        self.assertEqual(response.status_code, 200)
+    def heartQuery_test(self):
+        query = "SELECT COUNT(*) FROM heart_disease;"
+        cursor.execute(query)
+        result = cursor.fetchone()[0]
+        self.assertEqual(result, heart.shape[0])
 
-breakpoint()
 if __name__ == "__main__":
     unittest.main()
