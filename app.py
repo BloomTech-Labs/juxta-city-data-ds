f07c4dd50430021fde6f26715b0d3738dfd75b47
Ash827 <ashley.eddings89@gmail.com>
2020-07-06 21:56:46 -0400

    app.py complete

--- app.py
+++ app.py
@@ -1,144 +1,100 @@
-from flask import Flask, jsonify, request, json
+from flask import Flask, jsonify, render_template, request, json
+from flask import redirect, url_for
 from flask_cors import CORS
 import pandas as pd
+import glob,os
+from os import listdir
 
-df = pd.read_csv('./extra_datasets/final_0427.csv')
-photos = pd.read_csv('./extra_datasets/photos_big.csv')
-
-# JUXTA 2.0 Heart Disease data
-heart_disease = pd.read_csv('./extra_datasets/city_county_state.csv')
-
+city_df = pd.read_csv('./useful_datasets/reference.csv')
+photos = pd.read_csv('./notebooks_in_use/extraneous_datasets/photos_big.csv')
+economy = pd.read_csv('./useful_datasets/economy_data.csv')
+heart_disease = pd.read_csv('useful_datasets/heart_data.csv')
+housing = pd.read_csv('useful_datasets/housing_data.csv')
+jobs = pd.read_csv('useful_datasets/job_data.csv')
+location = pd.read_csv('useful_datasets/location_data.csv')
+people_stats = pd.read_csv('useful_datasets/people_stats_data.csv')
+original = pd.read_csv('./notebooks_in_use/extraneous_datasets/final_0427.csv') 
 
 app = Flask(__name__)
 CORS(app)
 
+# when using 'Global', we need to have the variable exist in the namespace
+state = None
+pop = None
+pop_ch = None
+med_age = None
+hdti = None
+rti = None
+pop_dense = None
+col = None
+drive = None
+aqi = None
+heart_health = None
 
 @app.route("/")
 def root():
     return ("hi")
 
 
-# Returns city name given a city id
-# Route structure: https://junta-test.herokuapp.com/name?id= [0-7127]
 @app.route('/name', methods=['GET', 'POST'])
-def id_name_only():
-    id = request.args['id']
-    city = df[df['id'] == int(id)]
-    output = city.to_dict('records')
-    name = output[0]['city']
-    return jsonify(name)
+def name():
+    city_list = city_df['City_Name'].to_list()
+    return jsonify(city_list)
 
 
-# # JUXTA 2.0 Starting endpoint for health-related data
-# # -------------
-# @app.route('/heart', methods=['GET', 'POST'])
-# def heart_data():
-#     cities = request.args['city']
-#     city = heart_disease[heart_disease['city']== cities]
-#     output = city.to_dict('records')
-#     return jsonify(output)
-
-# Returns only data for top 25 cities
-# Route structure: https://junta-test.herokuapp.com/top25
-@app.route('/top25', methods=['GET', 'POST'])
-def top_25_data():
-    output = photos.to_dict('records')
-    return jsonify(output)
+@app.route('/about', methods=['GET', 'POST'])
+def about_the_app():
+      return render_template('about.html')
 
 
-# Returns up to top 5 cities based on filters and livability score
-'''
-Route structure: http://junta-test.herokuapp.com/recommend?state=None&
-population=0&population_change=0&median_age=0&house_cost=0&rental_cost=0&
-population_density=0&cost_of_living=0&average_commute=0&air_quality=0
-[adjust values as appropriate]
-'''
 @app.route('/recommend', methods=['GET', 'POST'])
 def desc_and_livability():
     def recommend_cities():
-        global state
-        state = request.args['state']
-        global pop
-        pop = request.args['population']
-        global pop_ch
-        pop_ch = request.args['population_change']
-        global med_age
-        med_age = request.args['median_age']
-        global hdti
-        hdti = request.args['house_cost']
-        global rti
-        rti = request.args['rental_cost']
-        global pop_dense
-        pop_dense = request.args['population_density']
-        global col
-        col = request.args['cost_of_living']
-        global drive
-        drive = request.args['average_commute']
-        global aqi
-        aqi = request.args['air_quality']
-
-        state = list(state.split(', '))
-        pop = int(pop)
-        pop_ch = int(pop_ch)
-        med_age = int(med_age)
-        hdti = int(hdti)
-        rti = int(rti)
-        pop_dense = int(pop_dense)
-        col = int(col)
-        drive = int(drive)
-        aqi = int(aqi)
 
         def filter_dfs():
-            # Create "states" dataframe
+            # Create states dataframe
             multiple = []
 
-            if state != ['None']:
-                for item in state:
-                    frame = df[df['state'] == item]
-                    multiple.append(frame)
+            states = city_df['code']
 
-                    states = pd.concat(multiple[0:], ignore_index=True)
-            else:
-                states = df
-
-            # Create dataframe with filtered data
+            # Create people stats dataframe
             if pop != 0:
-                one = df[df['pop_rank'] == pop]
+                one = people_stats[people_stats['population'] == pop]
             else:
-                one = df
+                one = city_df
 
             if pop_ch != 0:
-                two = one[one['pop_ch_rank'] == pop_ch]
+                two = one[one['population_change'] == pop_ch]
             else:
                 two = one
 
             if med_age != 0:
-                three = two[two['med_age_rank'] == med_age]
+                three = two[two['Median_Age'] == med_age]
             else:
                 three = two
 
-            if hdti != 0:
-                four = three[three['house_cost'] == hdti]
+            if pop_dense != 0:
+                four = three[three['Population_Density'] == pop_dense]
             else:
                 four = three
 
-            if rti != 0:
-                five = four[four['rent_cost'] == rti]
+            if hdti != 0:
+                five = housing[housing['Median_House_Value'] == hdti]
             else:
                 five = four
 
-            if pop_dense != 0:
-                six = five[five['pop_density'] == pop_dense]
+            if rti != 0:
+                six = five[five['Median_Rent'] == rti]
             else:
                 six = five
 
             if col != 0:
-                seven = six[six['cli_index'] == col]
+                seven = six[six['Cost_of_Living_Index'] == col]
             else:
                 seven = six
 
             if drive != 0:
-                eight = seven[seven['avg_commute'] == drive]
+                eight = original[original['avg_commute'] == drive]
             else:
                 eight = seven
 
@@ -146,12 +102,17 @@ def desc_and_livability():
                 nine = eight[eight['air'] == aqi]
             else:
                 nine = eight
+                    
+            if heart_health != 0:
+                ten = heart_disease[heart_disease['scaled_heart_disease_deaths'] == heart_health]
+            else:
+                ten = nine
 
             # Create list of dataframes for use in case of no Top 5
-            dataframes = [one, two, three, four, five, six, seven, eight, nine]
+            dataframes = [one, two, three, four, five, six, seven, eight, nine, ten]
 
             # Merge dataframes to get fully filtered result
-            all = pd.merge(nine, states, on='city', how='inner')
+            all = pd.merge(ten, states, on='id', how='inner')
 
             # Sort by livability score and convert to a dictionary
             sort = all.sort_values(by='LivabilityScore_y', ascending=False)
@@ -160,7 +121,7 @@ def desc_and_livability():
             # Allow less than 5 results if necessary
             all_cities = []
             for item in range(len(dict)):
-                all_cities.append(dict[item]['city'])
+                all_cities.append(dict[item]['id'])
 
             top_5 = all_cities[0:5]
 
@@ -169,7 +130,7 @@ def desc_and_livability():
         def generate_cities():
             # Check that precise matches are available. If not, get imprecise matches.
             shapes = []
-            variables = [pop, pop_ch, med_age, hdti, rti, pop_dense, col, drive, aqi]
+            variables = [pop, pop_ch, pop_dense, med_age, hdti, rti, col, drive, aqi, heart_health]
 
             output, dataframes = filter_dfs()
 
@@ -179,10 +140,10 @@ def desc_and_livability():
                 for dataframe in dataframes:
                     shape = dataframe.shape
                     shapes.append(shape[0])
-                #Get the position number of the first dataframe that has zero rows
-                val = next((index for index, value in enumerate(shapes) if value == 0), None)
-                variables[val] = 0
-                return variables
+            #Get the position number of the first dataframe that has zero rows
+            val = next((index for index, value in enumerate(shapes) if value == 0), None)
+            variables[val] = 0
+            return variables
 
         def close_match():
 
@@ -195,26 +156,27 @@ def desc_and_livability():
                 pop = something[0]
                 global pop_ch
                 pop_ch = something[1]
-                global med_age
-                med_age = something[2]
+                global pop_dense
+                pop_dense = something[2]
                 global hdti
                 hdti = something[3]
                 global rti
                 rti = something[4]
-                global pop_dense
-                pop_dense = something[5]
+                global med_age
+                med_age = something[5]
                 global col
                 col = something[6]
-                global drive
+                global drive                
                 drive = something[7]
                 global aqi
                 aqi = something[8]
+                global heart_health
+                heart_health = something[9]
 
                 # Run generate_cities again with updated variable values
                 full_output = generate_cities()
 
                 return full_output
-
             else:
                 return something
 
@@ -229,75 +191,75 @@ def desc_and_livability():
                 else:
                     return again
             else:
-                 return info
+                return info
 
-        return get_results()
+            return get_results()
 
     # Return descriptions, id, and city name
-    top_cities = recommend_cities()
+    top_cities = city_df
 
     place_here = []
 
-    for x in top_cities:
+    for cities in top_cities:
 
         copy = pd.DataFrame()
-
-        city = df[df['city'] == x]
-        output = city.to_dict('records')
-        data = output[0]
+        city = city_df[city_df['City_Name'] == cities]
+        data = city_df
 
         # Basic
         id = data['id']
         name = data['City_Name']
-        name_and_state = data['city']
-        score = data['LivabilityScore']
+        name_and_state = original['city']
+        score = original['LivabilityScore']
 
         # Population
-        pop = data['population']
-        change = data['population_change']
-        density = data['Population_Density']
-        age = data['Median_Age']
+        pop = people_stats['population']
+        change = people_stats['population_change']
+        density = people_stats['Population_Density']
+        age = people_stats['Median_Age']
 
         # Economy
-        houseinc = data['Median_Income']
-        capitainc = data['per_capita_Income']
-        poverty = data['Percent_below_Poverty']
-        industry = data['Most_Common_Industries']
+        houseinc = economy['Median_Income']
+        capitainc = economy['per_capita_Income']
+        poverty = economy['Percent_below_Poverty']
+        industry = jobs['Most_Common_Industries']
 
         # Climate
-        aqi = data['AQI']
-        cold = data['Coldday_Count']
-        hot = data['Hotday_Count']
-        rain = data['Rainday_Count']
+        aqi = original['air']
+        cold = original['Coldday_Count']
+        hot = original['Hotday_Count']
+        rain = original['Rainday_Count']
 
         # Cost of Living
-        cli = data['Cost_of_Living_Index']
-        house = data['Median_House_Value']
-        hdti = data['HDTI']
-        tax = data['Property_taxes']
-        rent = data['Median_Rent']
-        rti = data['RTI']
-        drive = data['Average_Commute_Time']
+        cli = housing['Cost_of_Living_Index']
+        house = housing['Median_House_Value']
+        rent = housing['Median_Rent']
+        tax = housing['Property_taxes']
+        drive = original['avg_commute']
+
+        # Heart Disease information
+        heart_cases = heart_disease['normalized_heart_disease']
+        heart_deaths = heart_disease['scaled_heart_disease_deaths']
 
         # When relative words are needed
-        if change < 0:
+        if change.size < 0:
             growth = 'decrease'
         else:
             growth = 'increase'
 
-        if density < 1304:
+        if density.size < 1304:
             rise = 'lower'
         elif density == 1304:
             rise = 'equal to'
         else:
             rise = 'higher'
 
-        if cli < 51:
+        if cli.size < 51:
             cli_rel = 'low'
         else:
             cli_rel = 'high'
 
-        # Descriptions
+            # Descriptions
         copy['id'] = [f'{id}']
         copy['name'] = [f'{name}']
         copy['name_and_state'] = [f'{name_and_state}']
@@ -306,68 +268,83 @@ def desc_and_livability():
         copy['economy_desc'] = f'In {name}, the median household income is ${houseinc}. This means that, per person in the city, the average annual income is ${capitainc}. {poverty}% of people live below the poverty line. The most common industries are the following: {industry}.'
         copy['climate_desc'] = f'{name} has an air quality score of {aqi} - remember, lower is better! Last year, there were {cold} cold days, {hot} hot days, and {rain} rainy days.'
         copy['living_cost_desc'] = f"On a national scale, {name}'s cost of living is relatively {cli_rel}, with a score of {cli}. The median home value is ${house}, with a housing debt to income ratio of {hdti}. That includes the cost of property taxes, which is ${tax} on average. If you're a renter, you can expect a median cost of ${rent}, and the rent to income ratio is {rti}. Last but not least to consider, the average commute time is {drive} minutes."
+        copy['heart_disease_desc'] = f"{name} has {heart_deaths}% cases of heart disease within the city. This resulted in {heart_deaths} Heart Disease related deaths."
 
         output2 = copy.to_dict('records')
         descriptions = output2[0]
 
         place_here.append(descriptions)
-
     return jsonify(place_here)
 
 
-
-# Returns all data given a city name and state (format: City, State)
-# Route structure: https://junta-test.herokuapp.com/data?city= [City, State]
 @app.route('/data', methods=['GET', 'POST'])
 def all_data():
-    name = request.args['city']
-    city = df[df['city'] == name]
-    output = city.to_dict('records')
-    data = output[0]
+    
+    df1 = city_df
+    df2 = people_stats
+    df3 = economy
+    df4 = heart_disease
+    df5 = jobs
+    df6 = housing
+    df7 = location
+    df8 = original
+    merge1 = pd.merge(df1, df2, how = 'left', on = 'id')
+    merge2 = pd.merge(merge1, df4, how = 'left', on = 'id')
+    merge3 = pd.merge(merge2, df4, how = 'left', on = 'id')
+    merge4 = pd.merge(merge3, df5, how = 'left', on = 'id')
+    merge5 = pd.merge(merge4, df6, how = 'left', on = 'id')
+    merge6 = pd.merge(merge5, df7, how = 'left', on = 'id')
+    data = pd.merge(merge6, df8, how = 'left', on = 'id')
 
-    name = data['City_Name']
+    # Basic
+    id = city_df['id']
+    name = city_df['City_Name']
+    name_and_state = original['city']
+    score = original['LivabilityScore']
 
     # Population
-    pop = data['population']
-    change = data['population_change']
-    density = data['Population_Density']
-    age = data['Median_Age']
+    pop = people_stats['population']
+    change = people_stats['population_change']
+    density = people_stats['Population_Density']
+    age = people_stats['Median_Age']
 
     # Economy
-    houseinc = data['Median_Income']
-    capitainc = data['per_capita_Income']
-    poverty = data['Percent_below_Poverty']
-    industry = data['Most_Common_Industries']
+    houseinc = economy['Median_Income']
+    capitainc = economy['per_capita_Income']
+    poverty = economy['Percent_below_Poverty']
+    industry = jobs['Most_Common_Industries']
 
     # Climate
-    aqi = data['AQI']
-    cold = data['Coldday_Count']
-    hot = data['Hotday_Count']
-    rain = data['Rainday_Count']
+    aqi = original['air']
+    cold = original['Coldday_Count']
+    hot = original['Hotday_Count']
+    rain = original['Rainday_Count']
 
     # Cost of Living
-    cli = data['Cost_of_Living_Index']
-    house = data['Median_House_Value']
-    hdti = data['HDTI']
-    tax = data['Property_taxes']
-    rent = data['Median_Rent']
-    rti = data['RTI']
-    drive = data['Average_Commute_Time']
+    cli = housing['Cost_of_Living_Index']
+    house = housing['Median_House_Value']
+    rent = housing['Median_Rent']
+    tax = housing['Property_taxes']
+    drive = original['avg_commute']
+
+    # Heart Disease information
+    heart_cases = heart_disease['normalized_heart_disease']
+    heart_deaths = heart_disease['scaled_heart_disease_deaths']    
 
     # When relative words are needed
-    if change < 0:
+    if change.size < 0:
         growth = 'decrease'
     else:
         growth = 'increase'
 
-    if density < 1304:
+    if density.size < 1304:
         rise = 'lower'
-    elif density == 1304:
+    elif density.size == 1304:
         rise = 'equal to'
     else:
         rise = 'higher'
 
-    if cli < 51:
+    if cli.size < 51:
         cli_rel = 'low'
     else:
         cli_rel = 'high'
@@ -377,33 +354,19 @@ def all_data():
     data['economy_desc'] = f'In {name}, the median household income is ${houseinc}. This means that, per person in the city, the average annual income is ${capitainc}. {poverty}% of people live below the poverty line. The most common industries are the following: {industry}.'
     data['climate_desc'] = f'{name} has an air quality score of {aqi} - remember, lower is better! Last year, there were {cold} cold days, {hot} hot days, and {rain} rainy days.'
     data['living_cost_desc'] = f"On a national scale, {name}'s cost of living is relatively {cli_rel}, with a score of {cli}. The median home value is ${house}, with a housing debt to income ratio of {hdti}. That includes the cost of property taxes, which is ${tax} on average. If you're a renter, you can expect a median cost of ${rent}, and the rent to income ratio is {rti}. Last but not least to consider, the average commute time is {drive} minutes."
+    data['heart_disease_desc'] = f"{name} has {heart_deaths}% cases of heart disease within the city. This resulted in {heart_deaths} Heart Disease related deaths."
 
-    everything = data
+    everything = data.to_json()
 
     return jsonify(everything)
 
 
-# Returns list of cities that start with given characters
-# Route structure: https://junta-test.herokuapp.com/search?search= [Any characters]
-@app.route('/search', methods=['GET', 'POST'])
-def search_names():
-    input = request.args['search']
-    input = input.title()
-    cities = df[df['city'].str.startswith(input)]
-
-    ids = []
-    for index, row in cities.iterrows():
-        ids.append(index)
-
-    output = cities.to_dict('records')
-
-    all = []
-    for item in range(len(ids)):
-        city = output[item]['city']
-        all.append(city)
-
-    return jsonify(all)
+@app.route('/top25', methods=['GET', 'POST'])
+def top_25_data():
+    output = photos.to_dict('records')
+    return jsonify(output)
 
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
+
